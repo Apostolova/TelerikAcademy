e4578c92e99c5d4bde9228484f04f04fa52e20b5
Elena Apostolova <elena86@abv.bg>
2013-03-16 22:13:11 +0200

    add

---
+++ C_Charp/OOP/Home/2/Worker.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+     public class Worker : Humans
+    {
+        public decimal weekSalary;
+        public int workHoursPerDay;
+
+        public Worker(string firstName, string lastName, decimal weekSalary, int workHoursPerDay)
+            : base(firstName, lastName)
+        {
+            this.WeekSalary = weekSalary;
+            this.WorkHoursPerDay = workHoursPerDay;
+        }
+
+        public decimal WeekSalary
+        {
+            get
+            {
+                return this.weekSalary;
+            }
+            set
+            {
+                this.weekSalary = value;
+            }
+        }
+
+        public int WorkHoursPerDay
+        {
+            get
+            {
+                return this.workHoursPerDay;
+            }
+            set
+            {
+                this.workHoursPerDay = value;
+            }
+        }
+
+        public decimal MoneyPerHour()
+        {
+            decimal moneyPerHour = this.WeekSalary / (5 * this.WorkHoursPerDay);
+            return moneyPerHour;
+        }
+
+        
+    }
+}
